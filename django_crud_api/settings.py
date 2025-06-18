9722ba01baa74aa4ab7faffa531eda2352921073
CristhianCanar <cristhian.canar.m@gmail.com>
2025-06-17 22:52:48 -0500

    Se agrega url de cors, para frontend

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -143,7 +143,7 @@ if not DEBUG:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Cors authorization
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173","https://proyeccion.educarenemociones.com"]
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
